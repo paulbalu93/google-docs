dc4f9099b4754a534950a67823ef6869c6d83229
Paul Maliakel <paulbalu93@gmail.com>
2021-09-07 14:36:58 +0200

    adds header icons, searchbar

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,31 @@
 import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
 
 function Header() {
-  return <div className="flex items-center"></div>;
+  return (
+    <div className="flex items-center sticky top-0 z-50 px-4 py-2 shadow-md bg-white">
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className=" h-20 w-20 border-0"
+      >
+        <Icon name="menu" size="3xl" />
+      </Button>
+      <Icon name="description" size="5xl" color="blue" />
+      <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
+      <div className="flex flex-grow px-5 py-2 bg-gray-100 text-gray-600 rounded-lg ">
+        <Icon name="search" size="3xl" color="gray" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-grow px-5 bg-transparent text-base"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,17 @@ import "@material-tailwind/react/tailwind.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <>
-    <Head>
-      <link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet"
-      />
-    </Head>
-    <Component {...pageProps} />;
-  </>;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
